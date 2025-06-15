fd9c8d66a06f1d5c323042c8c9ca4c292141bc73
rawa08 <rawa_aref@hotmail.com>
2025-06-15 15:57:35 +0200

    chore(eslint): fix flat config plugin setup and add react settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
 import js from '@eslint/js';
 import * as tseslint from '@typescript-eslint/eslint-plugin';
 import parser from '@typescript-eslint/parser';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
 import importPlugin from 'eslint-plugin-import';
-import unusedImports from 'eslint-plugin-unused-imports';
+import unusedImportsPlugin from 'eslint-plugin-unused-imports';
 
 /** @type {import('eslint').FlatConfig[]} */
 export default [
@@ -28,22 +28,37 @@ export default [
         window: 'readonly',
         document: 'readonly',
         navigator: 'readonly',
+        React: true
       },
     },
     plugins: {
       '@typescript-eslint': tseslint,
-      react,
-      'react-hooks': reactHooks,
-      'jsx-a11y': jsxA11y,
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      'jsx-a11y': jsxA11yPlugin,
       import: importPlugin,
-      'unused-imports': unusedImports,
+      'unused-imports': unusedImportsPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
     rules: {
       semi: ['error', 'always'],
       'eol-last': ['error', 'always'],
       'no-console': 'warn',
       'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
       'unused-imports/no-unused-imports': 'warn',
+      'no-unused-vars': [
+        'error',
+        {
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
     },
   },
 ];
