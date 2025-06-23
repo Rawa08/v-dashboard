19101ff2d27af6b87df12ad08627637d75e8427a
rawa08 <rawa_aref@hotmail.com>
2025-06-23 16:17:15 +0200

    feat(types): add initial playlist and venue type definitions

---
+++ src/types/venue/index.ts
@@ -0,0 +1,38 @@
+export enum PlayListNameEnum {
+  SPORTS = 'Sports',
+  EVENTS = 'Events',
+  SPECIALS = 'Specials',
+  MENU = 'Menue'
+}
+
+export type PlaylistImage = {
+  id: string,
+  url: string,
+  playListId: string,
+  venueId?: string,
+  createdAt: Date,
+  createdByUserId: string,
+
+}
+
+export type PlayList = {
+  category: PlayListNameEnum,
+  id: string,
+  images: PlaylistImage[],
+  createdAt: Date,
+  createdByUserId: string,
+}
+
+export type PlayListsByCategory =
+  Record<PlayListNameEnum, PlayList>;
+
+export type Venue = {
+  id: string,
+  name: string,
+  city?: string,
+  address?: string,
+  phone?: string,
+  managerEmail?: string[],
+  devices: number,
+  playLists: PlayListsByCategory,
+};
