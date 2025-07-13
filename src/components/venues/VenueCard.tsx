9473f3a303c62ba48f3b707fa34b9b5a9b3c6fd8
rawa08 <rawa_aref@hotmail.com>
2025-07-13 19:31:23 +0200

    style(VenueCard): refine styling and HTML structure for venue managers display

--- src/components/venues/VenueCard.tsx
+++ src/components/venues/VenueCard.tsx
@@ -1,18 +1,25 @@
+import { useMemo } from 'react';
 import type { Venue } from '@/types/venue';
 
 const VenueCard = ({ venue }: { venue: Venue }) => {
+  const venueManagers = useMemo(() => {
+    const list = venue.venuePlaylistManagers
+      ? venue.venuePlaylistManagers.map((m) => (m.user.email))
+      : [];
+
+    return list.join(', ');
+  }, [venue]);
+
   return (
-      <li className="bg-white rounded shadow p-4 border border-gray-200 cursor-pointer hover:bg-gray-50 transition m-1 list-none">
-        <h2 className="text-lg font-semibold">{venue.name}</h2>
-        <p className="text-sm text-gray-600">
-          {venue.city && <>📍 {venue.city}, </>}
-          {venue.postalAddress && <>{venue.postalAddress}</>}
-        </p>
-        {venue.phone && <p className="text-sm text-gray-600">📞 {venue.phone}</p>}
-        {venue.venuePlaylistManagers && venue.venuePlaylistManagers?.length && (
-          venue?.venuePlaylistManagers.map((email) => (<p className="text-sm text-gray-600" key={email}>👤 Venue manager: {email}</p>))
-        )}
-      </li>
+    <li className="bg-white rounded shadow p-4 border border-gray-200 cursor-pointer hover:bg-gray-50 transition m-1 list-none">
+      <h2 className="text-lg font-semibold">{venue.name}</h2>
+      <p className="text-sm text-gray-600">
+        {venue.city && <>📍 {venue.city}, </>}
+        {venue.postalAddress && <>{venue.postalAddress}</>}
+      </p>
+      {venue.phone && <p className="text-sm text-gray-600">📞 {venue.phone}</p>}
+      <p className="text-sm text-gray-600">👤 Venue manager: {venueManagers}</p>
+    </li>
   );
 };
 
